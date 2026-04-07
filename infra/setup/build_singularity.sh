433f73ae6030a7b1e462e4f26e33ab99916a5284
Giuseppe Attanasio <giuseppeattanasio6@gmail.com>
2026-04-07 13:33:52 +0000

    Add .singularityignore file and update Singularity definition for improved build process

--- infra/setup/build_singularity.sh
+++ infra/setup/build_singularity.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-APPTAINER_TMPDIR=/mnt/scratch-artemis/giuseppe/tmp \
-  TMPDIR=/mnt/scratch-artemis/giuseppe/tmp \
+APPTAINER_TMPDIR=$SCRATCH/tmp TMPDIR=$SCRATCH/tmp \
   apptainer build \
   --bind /mnt/scratch-artemis/giuseppe/.cache/uv:/usr/local/share/uv/cache \
   /mnt/scratch-artemis/giuseppe/melt-data/melt_cuda126.sif infra/Singularity.def
